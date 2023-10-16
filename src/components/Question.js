330cfa3f1b926d3fea60d1bf9ba2cb2b2e3201f0
Robert Bubnik <rbubnik@gmail.com>
2023-10-16 12:49:41 -0400

    passing some tests

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+ useEffect(()=> {
+  const timer = setTimeout(()=> setTimeRemaining(timeRemaining -1), 1000)
+
+
+  if (timeRemaining == 0) {
+    setTimeRemaining(10);
+    onAnswered(false)
+  }
+
+  return function cleanup() {
+    clearInterval(timer)
+  }
+
+ }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
